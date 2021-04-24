3123afaed7bac3d495664c8f96f00ebc7c930fad
DUMBO <marie_dumbo@live.fr>
2021-04-24 17:18:06 +0200

    exo 1 terminé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'exo1';
+export class AppComponent implements OnInit {
+  nom: string;
+  police: string;
+  taille: string;
+  align: string;
+
+  ChangePolice(newPolice: string){
+    this.police = newPolice;
+  }
+
+  changeTaille(newTaille: number){
+    this.taille = newTaille+'px';
+  }
+
+  changeAlign(newAlign: string){
+    this.align = newAlign;
+  }
+
+  ngOnInit() {
+    this.police = 'arial';
+    this.taille= 24+'px';
+    this.align= 'left';
+  }
+
 }
